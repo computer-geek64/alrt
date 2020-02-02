64d9db5e4cc11ceee7a216ceac26c84ba829f9a8
computer-geek64 <sudouser512@gmail.com>
2020-02-02 00:47:47 -0500

    Update

--- src/test.py
+++ src/test.py
@@ -2,6 +2,13 @@
 # test.py
 
 from data import mongo
+from data import wildfires
+from data import gdacs
 
+#mongo.delete_user_documents(, "sample_user0")
+#mongo.add_user_documents([{"lat": 27, "lon": 30, "time": 1580599601.282248}], "sample_user0")
 
-mongo.delete_user_documents({}, "sample_user0")
+#gdacs.download_geojson()
+#documents = gdacs.get_disasters() + wildfires.get_wildfires()
+#mongo.add_disaster_documents(documents)
+#print(documents)
