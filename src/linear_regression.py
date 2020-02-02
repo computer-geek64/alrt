42fc3600d1f8b08ea71ddb19e33f261676913ef3
Pranav Pusarla <ppusarla@gmail.com>
2020-02-02 01:05:31 -0500

    Updated files

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 from numpy import genfromtxt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def predictPoint(latitudes, longitudes, radius):
     X = tf.placeholder("float")
@@ -14,7 +15,7 @@ def predictPoint(latitudes, longitudes, radius):
 
     learning_rate = 0.01
     epochs = 1000
-    n = 15
+    n = 120
 
     #Hypothesis or equation
     y_pred = tf.add(tf.multiply(X,W), b)
@@ -41,20 +42,27 @@ def predictPoint(latitudes, longitudes, radius):
         weight = session.run(W)
         bias = session.run(b)
 
-    latest_predicted_location = latitudes[14]*weight+bias
+    latest_predicted_location = latitudes[119]*weight+bias
+    print(latitudes[119], latest_predicted_location)
+    # x= latitudes
+    # y = latitudes*weight+bias
+    # plt.scatter(x,y)
+    # plt.show()
     degrees_of_latlong = radius/69.172
     change_in_y = math.sqrt((degrees_of_latlong)**2 / (1+(1/(weight**2))))
     change_in_x = change_in_y / weight
-    if (latitudes[0] < latitudes[14]):
-        new_latitude = latitudes[14] + change_in_x
+    if (latitudes[0] < latitudes[119]):
+        new_latitude = latitudes[119] + change_in_x
     else:
-        new_latitude = latitudes[14] - change_in_x
-    if (longitudes[0] < longitudes[14]):
-        new_longitude = longitudes[14] + change_in_y
+        new_latitude = latitudes[119] - change_in_x
+    if (longitudes[0] < longitudes[119]):
+        new_longitude = longitudes[119] + change_in_y
     else:
-        new_longitude = longitudes[14] - change_in_y
+        new_longitude = longitudes[119] - change_in_y
     return [new_latitude, new_longitude]
 
-# latitudes = np.array([29.652106, 29.652106, 29.652109, 29.652118, 29.652082, 29.652133, 29.652146, 29.652155, 29.652158, 29.652152, 29.652146, 29.652149, 29.652142, 29.652063, 29.652194])
-# longitudes = np.array([-82.350011, -82.350116, -82.350281, -82.35047, -82.350606, -82.350788, -82.350928, -82.351026, -82.351229, -82.351397, -82.351572, -82.351733, -82.35188, -82.352135, -82.352328])
-# print(predictPoint(latitudes, longitudes, 5))
+latitudes = np.array([29.652106, 29.652106, 29.652109, 29.652118, 29.652082, 29.652133, 29.652146, 29.652155, 29.652158, 29.652152, 29.652146, 29.652149, 29.652142, 29.652063, 29.652194])
+longitudes = np.array([-82.350011, -82.350116, -82.350281, -82.35047, -82.350606, -82.350788, -82.350928, -82.351026, -82.351229, -82.351397, -82.351572, -82.351733, -82.35188, -82.352135, -82.352328])
+plt.scatter(latitudes, longitudes)
+plt.show()
+print(predictPoint(latitudes, longitudes, 1))
